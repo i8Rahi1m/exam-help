db854b9e2cec2851981cee268c81595b1ab074a7
Ibrahim <ibrahimisme5@gmail.com>
2025-05-13 17:54:18 +0500

    style+nav

--- entities/collapseSection/ui.tsx
+++ entities/collapseSection/ui.tsx
@@ -1,30 +1,33 @@
-"use client";
+'use client';
 
 import React from 'react';
 import { CaretRightOutlined } from '@ant-design/icons';
 import { Collapse, theme } from 'antd';
 import type { CSSProperties } from 'react';
 import type { CollapseProps } from 'antd';
-import { Tickets } from '@/database/tickets'; // adjust the path as needed
+import { Tickets } from '@/database/tickets';
+import { useRouter } from 'next/navigation';
+import { LuPanelLeftOpen } from "react-icons/lu";
 
 const CollapseSection: React.FC = () => {
+  const router = useRouter();
   const { token } = theme.useToken();
 
   const panelStyle: CSSProperties = {
     marginBottom: 14,
-    fontSize: "20px",
+    fontSize: '20px',
     background: token.colorFillAlter,
-    borderRadius: token.borderRadiusLG,
-    border: 'none',
+    borderRadius: '14px',
+    border: 'none'
   };
 
   const parseQuestions = (ques: string) => {
     const parts = ques
       .split(/\n|\t/)
-      .filter((q) => q.trim() !== "");
+      .filter((q) => q.trim() !== '');
 
     return (
-      <div className="space-y-1 text-[17px]">
+      <div className="space-y-1 text-[17px] text-[white]">
         {parts.map((line, idx) => (
           <p key={idx}>
             <strong>{idx + 1}.</strong> {line.trim()}
@@ -34,11 +37,30 @@ const CollapseSection: React.FC = () => {
     );
   };
 
+  const handleNavigation = (id: number) => {
+    router.push(`/tickets/${id}`);
+  };
+
   const items: CollapseProps['items'] = Tickets[0].ticks.map((tick) => ({
     key: tick.id.toString(),
-    label: `Билет ${tick.id}`,
-    children: parseQuestions(tick.ques),
-    style: { ...panelStyle, borderLeft: `4px solid ${Tickets[0].color}`, background: '#ffffffdc' },
+    label: (
+      <div className="flex justify-between items-center text-white">
+        <span className='-ml-4.5'>Билет {tick.id}</span>
+        {/* Button for navigation */}
+        <button
+          onClick={() => handleNavigation(tick.id)}
+          className=" p-1 text-[22px] rounded-2xl hover:bg-[#80808056] cursor-pointer btndj"
+        >
+          <LuPanelLeftOpen/>
+        </button>
+      </div>
+    ),
+    children: (
+      <div>
+        {parseQuestions(tick.ques)}
+      </div>
+    ),
+    style: { ...panelStyle, border: "1px solid gray", borderLeft: `5px solid ${Tickets[0].color}`, background: 'black' },
   }));
 
   return (
@@ -49,7 +71,6 @@ const CollapseSection: React.FC = () => {
         expandIcon={({ isActive }) => (
           <CaretRightOutlined rotate={isActive ? 90 : 0} />
         )}
-        // className='bg-[#ffffffdc]'
         items={items}
       />
     </section>
