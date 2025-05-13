7b8d74cf79f4dea2a728955c651a97bdf8cf07ff
Ibrahim <ibrahimisme5@gmail.com>
2025-05-13 19:39:38 +0500

    shadow

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,8 @@ export default function RootLayout({
   return (
     <html lang="ru">
       <body>
+        <div className="fixed p-[150px] w-[200px] bg-[#ffffffdf] z-1 rounded-full shadow-[0px_0px_1000px] -left-[320px] -top-[150px]"></div>
+        <div className="fixed p-[150px] w-[200px] bg-[#ffffffdf] z-1 rounded-full shadow-[0px_0px_1000px] -right-[310px] -bottom-[200px]"></div>
         <p className="text-center text-[#bcbcbc]">Created by Ibrahim</p>
         {children}
       </body>
