d8c251d31d093f5f9d5066ebd02a588b93d54d3a
Ibrahim <ibrahimisme5@gmail.com>
2025-05-13 20:48:52 +0500

    home-btn

--- app/tickets/[id]/page.tsx
+++ app/tickets/[id]/page.tsx
@@ -1,12 +1,17 @@
 'use client';
 
+import Link from "next/link";
 // import { useParams } from 'next/navigation';
+import { AiFillHome } from "react-icons/ai";
 
 export default function TicketPage() {
   // const { id } = useParams();
 
   return (
     <div className="">
+      <Link href={"/"} className="transition-[0.3s] fixed top-3 left-2 p-1.5 text-[25px] rounded-2xl bg-[black] z-2 border-2 border-[gray] hover:bg-[#2c2c2c] after:shadow-2xl">
+        <AiFillHome/>
+      </Link>
     </div>
   );
 }
