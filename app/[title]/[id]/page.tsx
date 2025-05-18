97849c82d017a5b4cf24e159a7513cb454385f07
Ibrahim <ibrahimisme5@gmail.com>
2025-05-19 03:22:47 +0500

    database

--- app/[title]/[id]/page.tsx
+++ app/[title]/[id]/page.tsx
@@ -75,7 +75,7 @@ export default function TicketAnswerPage() {
         <span className='bg-black w-fit p-2 rounded-2xl border-[1px] text-[19px] mr-1.5' style={{ borderColor: category.color }}>{category.title.toUpperCase()}</span> - Билет {ticket.id}
       </h1>
       <div className="space-y-[40px] pt-3">
-        <div className='bg-[#3c3c3c99] p-4 rounded-3xl w-fit'>
+        <div className='bg-[#3c3c3c99] p-4 rounded-3xl'>
           <h2 className="text-lg font-semibold">Вопросы:</h2>
           <pre className="whitespace-pre-wrap -ml-2">{formatTextWithMarkers(ticket.ques)}</pre>
         </div>
