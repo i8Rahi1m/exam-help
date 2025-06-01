818dafc63d59b3ddda12fbe1a25d4619269a9e1c
Ibrahim <ibrahimisme5@gmail.com>
2025-06-01 14:52:48 +0500

    fix2

--- components/scrollButton/ui.tsx
+++ components/scrollButton/ui.tsx
@@ -21,7 +21,7 @@ export default function ScrollButton() {
     visible && (
       <button
         onClick={scrollToTop}
-        className="transition-[0.3s] fixed bottom-0 right-2 p-2 text-[20px] rounded-full bg-[black] z-10 border-[1px] border-[#ffffffc8] hover:bg-[#434343] hover:shadow-[0px_0px_8px_white] after:shadow-2xl active:bg-[#696969] active:scale-110"
+        className="transition-[0.3s] fixed bottom-2 right-3 p-2 text-[20px] rounded-full bg-[black] z-10 border-[1px] border-[#ffffffc8] hover:bg-[#434343] hover:shadow-[0px_0px_8px_white] after:shadow-2xl active:bg-[#696969] active:scale-110"
         aria-label="Scroll to top"
       >
         <ArrowUp className="w-6 h-6 text-white" />
