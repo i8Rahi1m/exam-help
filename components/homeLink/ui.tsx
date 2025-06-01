755f555297c1fdc904d6f62773df4ce3f310167c
Ibrahim <ibrahimisme5@gmail.com>
2025-06-01 13:34:03 +0500

    fix z-index

--- components/homeLink/ui.tsx
+++ components/homeLink/ui.tsx
@@ -8,7 +8,7 @@ export const HomeLink = () => {
     <div>
       <Link
         href={"/"}
-        className="transition-[0.3s] fixed top-2 left-3.5 p-2 text-[25px] rounded-full bg-[black] z-2 border-[1px] border-[#ffffffc8] hover:bg-[#434343] hover:shadow-[0px_0px_8px_white] after:shadow-2xl active:bg-[#696969] active:scale-110"
+        className="transition-[0.3s] fixed top-2 left-3.5 p-2 text-[25px] rounded-full bg-[black] z-10 border-[1px] border-[#ffffffc8] hover:bg-[#434343] hover:shadow-[0px_0px_8px_white] after:shadow-2xl active:bg-[#696969] active:scale-110"
       >
         <AiFillHome />
       </Link>
