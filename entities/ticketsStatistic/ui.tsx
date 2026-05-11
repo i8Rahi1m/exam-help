e69b8f7d71acbc7b3d1e0367367b35998fc35e0e
i8 <ibrahimisme5@gmail.com>
2026-05-11 23:35:38 +0500

    stat fix

--- entities/ticketsStatistic/ui.tsx
+++ entities/ticketsStatistic/ui.tsx
@@ -3,19 +3,29 @@
 import React from "react";
 import { Tickets } from "@/database/tickets";
 import Link from "next/link";
+import { useParams } from "next/navigation";
 
 const TicketsStatistic: React.FC = () => {
-  const oddTicks = Tickets[0].ticks.filter((tick) => tick.id % 2 !== 0);
-  const evenTicks = Tickets[0].ticks.filter((tick) => tick.id % 2 === 0);
+  const params = useParams();
+  const key = params?.title as string;
+
+  const category = Tickets.find((t) => t.key === key);
+
+if (!category) return null;
+
+const oddTicks = category.ticks.filter((t) => t.id % 2 !== 0);
+const evenTicks = category.ticks.filter((t) => t.id % 2 === 0);
 
   return (
     <section className="p-4 mt-[35px]">
       <div className="flex justify-between">
+
+        {/* ODD */}
         <div className="flex flex-col gap-3">
           {oddTicks.map((el) => (
             <Link
               key={el.id}
-              href={`/tickets/${Tickets[0].key}/${el.id}`}
+              href={`/tickets/${category.key}/${el.id}`}
               className="transition-[0.5s] p-3.5 w-[43vw] pr-0 flex justify-between h-[65px] items-center bg-[#131313] rounded-xl border-[1px] border-[gray] border-l-[6px] border-l-[#B0C4DE] hover:bg-[#000000ab] hover:shadow-[0px_0px_10px_gray] hover:scale-[1.02] active:bg-[#111111be] active:scale-[1.07] active:shadow-[0px_0px_11px_gray] inset-shadow-[0px_0px_20px] inset-shadow-[#000]"
             >
               <span
@@ -31,11 +41,13 @@ const TicketsStatistic: React.FC = () => {
             </Link>
           ))}
         </div>
+
+        {/* EVEN */}
         <div className="flex flex-col gap-3">
           {evenTicks.map((el) => (
             <Link
               key={el.id}
-              href={`/tickets/${Tickets[0].key}/${el.id}`}
+              href={`/tickets/${category.key}/${el.id}`}
               className="transition-[0.5s] p-3.5 pr-0 w-[43vw] flex justify-between h-[65px] items-center bg-[#131313] rounded-xl border-[1px] border-[gray] border-l-[6px] border-l-[#4981ea] hover:bg-[#000000ab] hover:shadow-[0px_0px_10px_gray] hover:scale-[1.02] active:bg-[#111111be] active:scale-[1.07] active:shadow-[0px_0px_11px_gray] inset-shadow-[0px_0px_20px] inset-shadow-[#000]"
             >
               <span
@@ -51,6 +63,7 @@ const TicketsStatistic: React.FC = () => {
             </Link>
           ))}
         </div>
+
       </div>
     </section>
   );
